54a6f5f0bb879fbb4435df937558726d071d16c7
Angel Daniel Naut Padilla <angel_naut@hotmail.com>
2024-07-04 10:02:12 -0400

    act

---
+++ BoletoBus.Infraestructure.Data/Logger/Services/LoggerService.cs
@@ -0,0 +1,13 @@
+﻿using BoletoBus.Infraestructure.Logger.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoletoBus.Infraestructure.Logger.Services
+{
+    public class LoggerService : ILoggerService
+    {
+    }
+}
---
+++ BoletoBus.Infraestructure.Data/Notification/Interfaces/INotificationService.cs
@@ -0,0 +1,14 @@
+﻿using BoletoBus.Infraestructure.Data.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoletoBus.Infraestructure.Data.Notification.Interfaces
+{
+    public interface INotificationService
+    {
+       
+    }
+}
